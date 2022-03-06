95633bf9abc31b25f83080c391e8a935f2323d1a
Laertes <maclellanj15@gmail.com>
2022-03-05 22:29:34 -0600

    added delete function

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,17 +1,26 @@
 const router = require("express").Router();
+const generateUniqueId = require("generate-unique-id");
 const { notes } = require("../../db/db.json");
-const { createNewNote, validateNote } = require("../../lib/notes");
+const { createNewNote, validateNote, findById, deleteNote } = require("../../lib/notes");
+
+
 
 router.get("/notes", (req, res) => {
     res.json(notes);
 });
 
+// router.get("/notes/:id", (req, res) => {
+//     const result = findById(req.params.id, notes);
+//     res.json(result);
+// });
+
 router.post("/notes", (req, res) => {
     // give notes an id
-    req.body.id = notes.length.toString();
-
-    // console log to test
-    console.log(req.body);
+    req.body.id = generateUniqueId({
+        length: 5,
+        useNumbers: true,
+        useLetters: false
+    });
     
     // validate data for notes
     if (!validateNote(req.body)) {
@@ -23,4 +32,9 @@ router.post("/notes", (req, res) => {
     }
 });
 
+router.delete("/notes/:id", (req, res) => {
+    const result = deleteNote(req.params.id, notes);
+    res.json(result);
+    console.log("success!");
+});
 module.exports = router;
